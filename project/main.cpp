77683a7ec0d2e7467c65fc67ce38cc80832ac8e5
mao <maocutiee@gmail.com>
2025-01-07 20:44:29 +0300

    07.01.25 opcode fixed for wallhack

--- project/main.cpp
+++ project/main.cpp
@@ -51,55 +51,6 @@ void SetRandomTitleThread() {
 }
 #pragma endregion
 
-#pragma region Blur
-typedef enum WINDOWCOMPOSITIONATTRIB {
-    WCA_UNDEFINED,
-    WCA_NCRENDERING_ENABLED,
-    WCA_NCRENDERING_POLICY,
-    WCA_TRANSITIONS_FORCEDISABLED,
-    WCA_ALLOW_NCPAINT,
-    WCA_CAPTION_BUTTON_BOUNDS,
-    WCA_NONCLIENT_RTL_LAYOUT,
-    WCA_FORCE_ICONIC_REPRESENTATION,
-    WCA_EXTENDED_FRAME_BOUNDS,
-    WCA_HAS_ICONIC_BITMAP,
-    WCA_THEME_ATTRIBUTES,
-    WCA_NCRENDERING_EXILED,
-    WCA_NCADORNMENTINFO,
-    WCA_EXCLUDED_FROM_LIVEPREVIEW,
-    WCA_VIDEO_OVERLAY_ACTIVE,
-    WCA_FORCE_ACTIVEWINDOW_APPEARANCE,
-    WCA_DISALLOW_PEEK,
-    WCA_CLOAK,
-    WCA_CLOAKED,
-    WCA_ACCENT_POLICY,
-    WCA_FREEZE_REPRESENTATION,
-    WCA_EVER_UNCLOAKED,
-    WCA_VISUAL_OWNER,
-    WCA_LAST,
-} WINDOWCOMPOSITIONATTRIB;
-typedef struct WINDOWCOMPOSITIONATTRIBDATA {
-    WINDOWCOMPOSITIONATTRIB Attrib;
-    PVOID pvData;
-    SIZE_T cbData;
-} WINDOWCOMPOSITIONATTRIBDATA;
-typedef enum ACCENT_STATE {
-    ACCENT_DISABLED,
-    ACCENT_ENABLE_GRADIENT,
-    ACCENT_ENABLE_TRANSPARENTGRADIENT,
-    ACCENT_ENABLE_BLURBEHIND,
-    ACCENT_INVALID_STATE
-} ACCENT_STATE;
-typedef struct ACCENT_POLICY {
-    ACCENT_STATE AccentState;
-    DWORD AccentFlags;
-    DWORD GradientColor;
-    DWORD AnimationId;
-} ACCENT_POLICY;
-typedef BOOL(WINAPI* pfnSetWindowCompositionAttribute)(HWND, WINDOWCOMPOSITIONATTRIBDATA*);
-static auto SetWindowCompositionAttribute = (pfnSetWindowCompositionAttribute)GetProcAddress(LoadLibrary("user32.dll"), "SetWindowCompositionAttribute");
-#pragma endregion
-
 #pragma region ASCII
 void SetConsoleColor(WORD color) {
     HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
@@ -125,37 +76,37 @@ void AsciiName() {
 }
 #pragma endregion
 
-#pragma region Auth
-bool AuthenticateUser() {
-    AsciiName();
-    std::string key;
-    std::cout << skCrypt("[ AUTH ] Enter license: ");
-    std::cin >> key;
-
-    KeyAuthApp.init();
-
-    KeyAuthApp.license(key);
-
-    if (KeyAuthApp.response.success) {
-        std::cout << skCrypt("[ AUTH ] Authorization successful.\n");
-        Sleep(400);
-        return true;
-    }
-    else {
-        std::cerr << skCrypt("[ AUTH ] Invalid key or access has expired.\n");
-        Sleep(1000);
-        return false;
-    }
-}
-#pragma endregion
+//#pragma region Auth
+//bool AuthenticateUser() {
+//    AsciiName();
+//    std::string key;
+//    std::cout << skCrypt("[ AUTH ] Enter license: ");
+//    std::cin >> key;
+//
+//    KeyAuthApp.init();
+//
+//    KeyAuthApp.license(key);
+//
+//    if (KeyAuthApp.response.success) {
+//        std::cout << skCrypt("[ AUTH ] Authorization successful.\n");
+//        Sleep(400);
+//        return true;
+//    }
+//    else {
+//        std::cerr << skCrypt("[ AUTH ] Invalid key or access has expired.\n");
+//        Sleep(1000);
+//        return false;
+//    }
+//}
+//#pragma endregion
 
 #pragma region Menu
 void cheatMenu() {
     AsciiName();
-    std::cout << skCrypt("\n[ F ] WallHack") << std::endl;
-    std::cout << skCrypt("\n[ Z ] Chams") << std::endl;
-    std::cout << skCrypt("\n[ X ] WallHack v2") << std::endl;
-    std::cout << skCrypt("\n[ C ] Delete Effects on screen") << std::endl;
+    std::cout << skCrypt("\n[ F ] WallHack [ TEST ]") << std::endl;
+    std::cout << skCrypt("\n[ Z ] Chams [ CRASH GAME ]") << std::endl;
+    std::cout << skCrypt("\n[ X ] Delete Effects") << std::endl;
+    std::cout << skCrypt("\n[ C ] WallHack") << std::endl;
 }
 #pragma endregion
 
@@ -165,15 +116,6 @@ int main()
     HWND hWnd = GetConsoleWindow();
     HANDLE hStd = GetStdHandle(STD_OUTPUT_HANDLE);
 
-    if (SetWindowCompositionAttribute) {
-        ACCENT_POLICY accent = { ACCENT_ENABLE_BLURBEHIND, 0, 0, 0 };
-        WINDOWCOMPOSITIONATTRIBDATA data;
-        data.Attrib = WCA_ACCENT_POLICY;
-        data.pvData = &accent;
-        data.cbData = sizeof(accent);
-        SetWindowCompositionAttribute(hWnd, &data);
-    }
-
     // Remove scroll bar
     CONSOLE_SCREEN_BUFFER_INFO csbi;
     GetConsoleScreenBufferInfo(hStd, &csbi);
@@ -182,11 +124,12 @@ int main()
 
     std::thread titleThread(SetRandomTitleThread);
     titleThread.detach();
-    if (!AuthenticateUser()) {
-        std::cout << skCrypt("[ DEBUG ] Login ERROR") << std::endl;
-        Sleep(1000);
-        return 0;
-    }
+
+    //if (!AuthenticateUser()) {
+    //    std::cout << skCrypt("[ DEBUG ] Login ERROR") << std::endl;
+    //    Sleep(1000);
+    //    return 0;
+    //}
 
     DWORD pid = pidder::get_process_id("stalcraft.exe");
     if (!pid) {
@@ -202,7 +145,7 @@ int main()
     memory.processHandle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pid);
 
     if (!memory.processHandle) {
-        std::cerr << skCrypt("\n\t[ ERROR ] Failed to open process with sufficient privileges!") << std::endl;
+        std::cerr << skCrypt("\n\t[ ERROR ] Failed to open process!") << std::endl;
         return -1;
     }
 
@@ -223,18 +166,18 @@ int main()
         if (baseAddressFound) break;
     } while (true);
 
-    //Offsets
-    /*
-       https://youtu.be/j9dK4v_z8WA
-    */
-    std::uintptr_t wallhackAddress = baseAddress + 0x7BC5B8;// Updated
-    std::uintptr_t chamsAddress = baseAddress + 0x7B451C; // Outdated
+    
+    /* OFFSETS */
+    /* https://youtu.be/j9dK4v_z8WA */
+    
+    std::uintptr_t wallhackAddress = baseAddress + 0x7BC5B8;
+    std::uintptr_t chamsAddress = baseAddress + 0x7BBEB8;
 
     system("cls");
     cheatMenu();
     while (true)
     {
-        if (GetAsyncKeyState(0x5A) & 0x8000) //F
+        if (GetAsyncKeyState(0x5A) & 0x8000) // - Z
         {
             BOOL result;
             SIZE_T bytesWritten;
@@ -244,11 +187,11 @@ int main()
                 result = WriteProcessMemory(memory.processHandle, (LPVOID)wallhackAddress, wallhackInstruction, sizeof(wallhackInstruction), &bytesWritten);
                 if (result && bytesWritten == sizeof(wallhackInstruction))
                 {
-                    //std::cout << "Instruction reverted to original." << std::endl;
+                    //std::cout << "modified" << std::endl;
                 }
                 else
                 {
-                    std::cerr << "Failed to revert. Error: " << GetLastError() << std::endl;
+                    std::cerr << "Failed to modify. Error: " << GetLastError() << std::endl;
                 }
             }
             else
@@ -257,7 +200,7 @@ int main()
 
                 if (result && bytesWritten == sizeof(newWallHackInstruction))
                 {
-                    //std::cout << "Instruction modified to shl r9d,04." << std::endl;
+                    //std::cout << "modified" << std::endl;
                 }
                 else
                 {
@@ -267,7 +210,7 @@ int main()
             wallhackEnabled = !wallhackEnabled;
         }
         Sleep(50);
-        if (GetAsyncKeyState(0x58) & 0x8000) //Z
+        if (GetAsyncKeyState(0x58) & 0x8000) // - X
         {
             BOOL result;
             SIZE_T bytesWritten;
@@ -277,11 +220,11 @@ int main()
                 result = WriteProcessMemory(memory.processHandle, (LPVOID)chamsAddress, chamsInstruction, sizeof(chamsInstruction), &bytesWritten);
                 if (result && bytesWritten == sizeof(chamsInstruction))
                 {
-                    //std::cout << "Instruction reverted to original." << std::endl;
+                    //std::cout << "modified" << std::endl;
                 }
                 else
                 {
-                    std::cerr << skCrypt("Failed to revert. Error: ") << GetLastError() << std::endl;
+                    std::cerr << skCrypt("Failed to modify. Error: ") << GetLastError() << std::endl;
                 }
             }
             else
@@ -290,7 +233,7 @@ int main()
 
                 if (result && bytesWritten == sizeof(newChamsInstruction))
                 {
-                    //std::cout << "Instruction modified to shl r9d,04." << std::endl;
+                    //std::cout << "modified" << std::endl;
                 }
                 else
                 {
@@ -300,7 +243,7 @@ int main()
             chamsEnabled = !chamsEnabled;
         }
         Sleep(50);
-        if (GetAsyncKeyState(0x58) & 0x8000) //X
+        if (GetAsyncKeyState(0x43) & 0x8000) // - C
         {
             BOOL result;
             SIZE_T bytesWritten;
@@ -310,11 +253,11 @@ int main()
                 result = WriteProcessMemory(memory.processHandle, (LPVOID)wallhackAddress, wallhackInstruction, sizeof(wallhackInstruction), &bytesWritten);
                 if (result && bytesWritten == sizeof(wallhackInstruction))
                 {
-                    //std::cout << "Instruction reverted to original." << std::endl;
+                    //std::cout << "modified" << std::endl;
                 }
                 else
                 {
-                    std::cerr << "Failed to revert. Error: " << GetLastError() << std::endl;
+                    std::cerr << "Failed to modify. Error: " << GetLastError() << std::endl;
                 }
             }
             else
@@ -323,7 +266,7 @@ int main()
 
                 if (result && bytesWritten == sizeof(newWallhackInstructionv2))
                 {
-                    //std::cout << "Instruction modified to shl r9d,04." << std::endl;
+                    //std::cout << "modified" << std::endl;
                 }
                 else
                 {
@@ -333,7 +276,7 @@ int main()
             wallhackEnabledv2 = !wallhackEnabledv2;
         }
         Sleep(50);
-        if (GetAsyncKeyState(0x43) & 0x8000) //C
+        if (GetAsyncKeyState(0x46) & 0x8000) // - F
         {
             BOOL result;
             SIZE_T bytesWritten;
@@ -343,11 +286,11 @@ int main()
                 result = WriteProcessMemory(memory.processHandle, (LPVOID)chamsAddress, chamsInstruction, sizeof(chamsInstruction), &bytesWritten);
                 if (result && bytesWritten == sizeof(chamsInstruction))
                 {
-                    //std::cout << "Instruction reverted to original." << std::endl;
+                    //std::cout << "modified" << std::endl;
                 }
                 else
                 {
-                    std::cerr << skCrypt("Failed to revert. Error: ") << GetLastError() << std::endl;
+                    std::cerr << skCrypt("Failed to modify. Error: ") << GetLastError() << std::endl;
                 }
             }
             else
@@ -356,7 +299,7 @@ int main()
 
                 if (result && bytesWritten == sizeof(deleteEffectsInstruction))
                 {
-                    //std::cout << "Instruction modified to shl r9d,04." << std::endl;
+                    //std::cout << "modified" << std::endl;
                 }
                 else
                 {
